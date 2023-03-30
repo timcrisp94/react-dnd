b864cfbc0fa4021213fec89f23838ce105f03ef5
Tim Crisp <timothycrisp@gmail.com>
2023-03-29 20:34:06 -0500

    add navItems array to app js, clean up return; clear up react css, add nav bar css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+
 import './App.css';
 
 function App() {
+  const [navItems, setNavItems] = useState([
+    {url: "/class-list", name: "Class List"},
+    {url: "/monster-list", name: "Scary Monsters"},
+    {url: "/spell-search", name: "Search for Spells"}
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>
+        This is our app!
+      </h1>
+    </>
   );
 }
 
