00664ebf90f92f32432833fe37d4ab9fad06c7e5
Tim Crisp <timothycrisp@gmail.com>
2023-04-18 19:40:38 -0500

    connect api to class list

--- src/pages/ClassList/ClassList.jsx
+++ src/pages/ClassList/ClassList.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { getClassList } from '../../services/api-calls'
 
 const ClassList = (props) => {
-    const [results, setResults] = useState(['Paladin', 'Bard', 'Rogue'])
+    const [classes, setClasses] = useState([])
+
+    useEffect(() => {
+        getClassList()
+        .then(classData => setClasses(classData.results))
+    }, [])
     return (
         <>
             <div>
-                <h3>I'm the class list</h3>
+                <h3>Class List</h3>
+                <div className="icon-container">
+                {classes.map((classTitle) => (
+                    <div id="classDiv" key={classTitle.index}>
+                        <img 
+                            style={{ width: "100px", height: "100px" }}
+                            src={`/images/${classTitle.name}.svg`} 
+                            alt="class-logo"
+                        />
+                {classTitle.name}
             </div>
-        </>
+          ))}
+        </div>
+      </div>
+    </>
     )
 }
 
